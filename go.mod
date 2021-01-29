85b3774df6b1e75555fd03daee311c57d3e74221
DoLim <limdongyoung0@gmail.com>
2021-01-29 17:43:59 +0900

    study: Made todo example code, referenced graphql-go

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/graphql-go-study
+module github.com/d0lim/graphql-go-study
 
 go 1.15
 
-require github.com/graphql-go/graphql v0.7.9
+require (
+	github.com/graphql-go/graphql v0.7.9
+	github.com/graphql-go/handler v0.2.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,41 +1,60 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
+	"math/rand"
+	"net/http"
+	"time"
 
 	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
+
+	"github.com/d0lim/graphql-go-study/schema"
 )
 
-func main() {
-	// Schema
-	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world", nil
-			},
-		},
-	}
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, err := graphql.NewSchema(schemaConfig)
-	if err != nil {
-		log.Fatalf("failed to create new schema, error: %v", err)
-	}
+func init() {
+	todo1 := schema.Todo{ID: "a", Text: "A todo not to forget", Done: false}
+	todo2 := schema.Todo{ID: "b", Text: "This is the most important", Done: false}
+	todo3 := schema.Todo{ID: "c", Text: "Please do this or else", Done: false}
+	schema.TodoList = append(schema.TodoList, todo1, todo2, todo3)
 
-	// Query
-	query := `
-		{
-			hello
-		}
-	`
-	params := graphql.Params{Schema: schema, RequestString: query}
-	r := graphql.Do(params)
-	if len(r.Errors) > 0 {
-		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	rand.Seed(time.Now().UnixNano())
+}
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("Wrong result, unexpected errors: %v", result.Errors)
 	}
-	rJSON, _ := json.Marshal(r)
-	fmt.Printf("%s \n", rJSON) // {"data":{"hello":"world"}}
+	return result
+}
+
+func main() {
+
+	h := handler.New(&handler.Config{
+		Schema:     &schema.TodoSchema,
+		Pretty:     true,
+		GraphiQL:   false,
+		Playground: true,
+	})
+	// http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+	// 	result := executeQuery(r.URL.Query().Get("query"), schema.TodoSchema)
+	// 	json.NewEncoder(w).Encode(result)
+	// })
+	// // Serve static files
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/graphql", h)
+	http.Handle("/", fs)
+	// Display some basic instructions
+	// fmt.Println("Now server is running on port 8080")
+	// fmt.Println("Get single todo: curl -g 'http://localhost:8080/graphql?query={todo(id:\"b\"){id,text,done}}'")
+	// fmt.Println("Create new todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{createTodo(text:\"My+new+todo\"){id,text,done}}'")
+	// fmt.Println("Update todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{updateTodo(id:\"a\",done:true){id,text,done}}'")
+	// fmt.Println("Load todo list: curl -g 'http://localhost:8080/graphql?query={todoList{id,text,done}}'")
+	// fmt.Println("Access the web app via browser at 'http://localhost:8080'")
+
+	http.ListenAndServe(":5000", nil)
 }
