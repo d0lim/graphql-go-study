661eab7c957a198070d230f8ddb48773244acc93
DoLim <limdongyoung0@gmail.com>
2021-01-29 17:45:10 +0900

    chore: cleaned code

--- main.go
+++ main.go
@@ -40,21 +40,10 @@ func main() {
 		GraphiQL:   false,
 		Playground: true,
 	})
-	// http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
-	// 	result := executeQuery(r.URL.Query().Get("query"), schema.TodoSchema)
-	// 	json.NewEncoder(w).Encode(result)
-	// })
-	// // Serve static files
+
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/graphql", h)
 	http.Handle("/", fs)
-	// Display some basic instructions
-	// fmt.Println("Now server is running on port 8080")
-	// fmt.Println("Get single todo: curl -g 'http://localhost:8080/graphql?query={todo(id:\"b\"){id,text,done}}'")
-	// fmt.Println("Create new todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{createTodo(text:\"My+new+todo\"){id,text,done}}'")
-	// fmt.Println("Update todo: curl -g 'http://localhost:8080/graphql?query=mutation+_{updateTodo(id:\"a\",done:true){id,text,done}}'")
-	// fmt.Println("Load todo list: curl -g 'http://localhost:8080/graphql?query={todoList{id,text,done}}'")
-	// fmt.Println("Access the web app via browser at 'http://localhost:8080'")
 
 	http.ListenAndServe(":5000", nil)
 }
